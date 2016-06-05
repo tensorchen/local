208edcc0ee000921832eb0dde8debd4c5f93060c
tensorchen <tensorchen@icloud.com>
2016-06-05 14:52:02 +0800

    vimrc init commit

---
+++ etc/vim/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#
+# @author: tensorchen
+# @time: 2016年06月05日
+#
+
+VIM_HOME=/local/etc/vim
+
+ln -s ${HOME}${VIM_HOME}/vim   ${HOME}/.vim
+ln -s ${HOME}${VIM_HOME}/vimrc ${HOME}/.vimrc
---
+++ etc/vim/uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# @author: tensorchen
+# @time: 2016年06月05日
+#
+
+rm -i ${HOME}/.vimrc
+rm -i ${HOME}/.vim
